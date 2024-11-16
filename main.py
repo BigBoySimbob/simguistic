0230684f3d2bd4bc2d47f2d5e464e5573f0e6787
Sim <20pim@live.co.uk>
2024-11-16 14:38:22 +0100

    Fixes

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from wordlist_utils import get_word_counts
 
 app = Flask(
     __name__,
-    template_folder='/templates',
-    static_folder='/static',
+    template_folder='simguistic/templates',
+    static_folder='simguistic/static',
     static_url_path='/simguistic/static'
 )
 app.secret_key = 'your_secret_key'  # Use a secure random key in production
